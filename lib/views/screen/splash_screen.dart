5663f2a9a3cd373c8e482dabda76815acd32992f
tanvir-hridoy <ikramulhasantanvir@gmail.com>
2025-07-07 15:00:09 +0600

    added custom widgets

--- lib/views/screen/splash_screen.dart
+++ lib/views/screen/splash_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:towservice/global/custom_assets/assets.gen.dart';
-import 'package:towservice/utils/app_icons.dart';
-import 'package:towservice/utils/app_image.dart';
+import 'package:towservice/widgets/custom_text.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -10,13 +8,45 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        physics: NeverScrollableScrollPhysics(),
-        child: Column(
-          children: [
-            Assets.images.splashImage.image(),
-          ],
-        ),
+      body: Stack(
+        children: [
+          // Background Image
+          Positioned.fill(
+            child: Assets.images.splashImage.image(
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          // Centered Icon (SVG)
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // Splash Icon
+                Assets.icons.splashIcon.svg(height: 120, width: 120),
+
+                const SizedBox(height: 20),
+
+                // Title
+                const CustomText(
+                  text: 'Tow Services',
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+
+                const SizedBox(height: 8),
+
+                // Subtitle or secondary text
+                const CustomText(
+                  text: 'We care for your journey',
+                  fontSize: 16,
+                  color: Colors.white70,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
